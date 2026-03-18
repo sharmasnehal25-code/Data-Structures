667729fb47b3d555f64f2d19b0abb72af6d13165
sharmasnehal25-code <sharmasnehal259@gmail.com>
2026-03-18 10:30:21 +0530

    Create matrix addition.c

---
+++ matrix addition.c
@@ -0,0 +1,220 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,sum[3][3];
+	int a[3][3],b[3][3];
+
+	printf("Enter elements in matrix A");
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++)
+	{
+		scanf("%d",&a[i][j]);	
+	}	
+	}
+	printf("Enter elements in matrix B");
+		for(i=0;i<3;i++)
+		{
+	for(j=0;j<3;j++)
+	{
+	scanf("%d",&b[i][j]);	
+	}	
+	}
+	for(i=0;i<3;i++)
+	{
+	for(j=0;j<3;j++)
+	{
+	sum[i][j]=a[i][j]+b[i][j];}
+}
+	printf("Addition of matrix is:\n");
+		for(i=0;i<3;i++)
+	{
+	for(j=0;j<3;j++)
+	{
+printf("%d",sum[i][j]);
+}	
+}
+	return 0;
+	}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
